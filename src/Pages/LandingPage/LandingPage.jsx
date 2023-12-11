15ed22b0c615ffc47c23226ae37878f31f6248ad
Yogesh Kumar <40662948+MACHINE-IT@users.noreply.github.com>
2023-12-11 11:11:25 +0530

    Updated backend get api in LandingPage.jsx
    Updated backend get api in LandingPage.jsx

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -9,7 +9,7 @@ const LandingPage = () => {
 
     const fetchData = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/');
+            const response = await axios.get('https://cipher-backend-1qvm.onrender.com/');
             setUsersList(response.data);
         } catch (error) {
             console.error('Error fetching data:', error);
